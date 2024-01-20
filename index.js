e38778562fdee4617391e48d95ab87ecf8d80e69
Rachael Momber <rlmomber@gmail.com>
2024-01-20 15:14:47 +0900

    add event listener to change quote on click

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const URL = 'https://api.kanye.rest';
 
+window.onload=function(){
+  const buttonElement = document.getElementById('kanye-button');
+  buttonElement.addEventListener('click', (event) => {
+    import_quote();
+    click_sound();
+  })
+}
+
 function import_quote() {
   fetch(URL)
     .then(response => response.json())
